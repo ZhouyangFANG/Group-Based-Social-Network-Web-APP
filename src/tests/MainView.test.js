2ae64af92623cda0b89243d1410704983b017d5b
Zhouyang Fang <fangzhy1998@gmail.com>
2021-11-15 16:37:34 -0500

    delete all tests

--- src/tests/MainView.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import MainView from '../ReactComponents/MainView';
-
-test('renders learn react link', () => {
-  render(<MainView />);
-  const element = screen.getByText(/Public Groups/i);
-  expect(element).toBeInTheDocument();
-});
