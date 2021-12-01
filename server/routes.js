a5bdb0d5b2f4853d0547a5c839843bdbd84e81fe
YuchenWang99 <yuchen.wang1220@gmail.com>
2021-12-01 17:40:59 -0500

    auth done

--- server/routes.js
+++ server/routes.js
@@ -3,6 +3,11 @@ const sha256 = require('js-sha256');
 require('dotenv').config();
 uuid = require('uuid');
 
+const crypto = require('crypto');
+const algorithm = 'aes-256-ctr';
+const secretKey = 'xBLCvFTxhjkqjYTC2ynYuSVg3o6YMB1j';
+const iv = 'blahblahblahblah';
+
 const connection = mysql.createConnection({
   host: process.env.rds_host,
   user: process.env.rds_user,
@@ -12,13 +17,58 @@ const connection = mysql.createConnection({
 });
 connection.connect();
 
-async function postUser(req, res){
+async function checkCookie(req, res, next){
+    const cookie = req.cookies.token;
+    const decipher = crypto.createDecipheriv(algorithm, secretKey, iv);
+
+    const decrpyted = Buffer.concat([decipher.update(Buffer.from(cookie, 'hex')), decipher.final()]).toString();
+    let userInfo;
+    try{
+      userInfo = JSON.parse(decrpyted);
+    } catch (e) {
+      res.status(400);
+      res.json({ message: 'tampered cookie' })
+      return;
+    }
+
+    const username = userInfo.username;
+    const password = userInfo.password;
+
+    if (!username || !password){
+      res.status(400);
+      res.json({ message: 'tampered cookie' })
+      return;
+    } else {
+      connection.query(`SELECT * from user where 
+      username ='${username}';`, function (error, results, fields) {
+        if (error) {
+          res.status(400)
+          res.json({ message: 'tampered cookie', error: error })
+        } else if (results.length !== 1) {
+          res.status(400)
+          res.json({message: 'tampered cookie, No such user'})
+        } else {
+            if (results[0].username === username && results[0].password === sha256(password)){
+              req.userInfo = results[0];
+              next()
+            } else {
+              res.status(400)
+              res.json({message: 'tampered cookie, password incorrect'})  
+            }
+        }
+      });
+    }
+
+
+}
+
+async function createUser(req, res){
     const username = req.body.username;
     const password = req.body.password;
     const passwordHash = sha256(password);
     const uuidv4 = uuid.v4();
-    connection.query(`INSERT INTO  user(id, username, password)
-    values ('${uuidv4}', '${username}', '${passwordHash}');`, function (error, results, fields) {
+    connection.query(`INSERT INTO  user(id, username, password, registerDate)
+    values ('${uuidv4}', '${username}', '${passwordHash}', '${new Date().toISOString().slice(0, 10)}');`, function (error, results, fields) {
       if (error) {
         if(error.code === 'ER_DUP_ENTRY'){
           res.status(409)
@@ -29,6 +79,19 @@ async function postUser(req, res){
         }
       } else if (results) {
         res.status(201)
+        const cipher = crypto.createCipheriv(algorithm, secretKey, iv);
+        const tokenJson = {
+          'username': username,
+          'password': password
+        };
+        const tokenString = JSON.stringify(tokenJson);
+        const encrypted = Buffer.concat([cipher.update(tokenString), cipher.final()]).toString('hex');
+        res.cookie(`token`,encrypted,{
+          maxAge: 24 * 60 * 60 * 1000,
+          secure: false,
+          httpOnly: true,
+          sameSite: 'lax'
+        });
         res.json({ id: uuidv4, username: username, password: passwordHash})
       }
     });
@@ -47,6 +110,19 @@ async function loginUser(req, res){
         res.json('No such user')
       } else {
           if (results[0].username === username && results[0].password === sha256(password)){
+            const cipher = crypto.createCipheriv(algorithm, secretKey, iv);
+            const tokenJson = {
+              'username': username,
+              'password': password
+            };
+            const tokenString = JSON.stringify(tokenJson);
+            const encrypted = Buffer.concat([cipher.update(tokenString), cipher.final()]).toString('hex');
+            res.cookie(`token`,encrypted,{
+              maxAge: 24 * 60 * 60 * 1000,
+              secure: false,
+              httpOnly: true,
+              sameSite: 'lax'
+            });
             res.status(200)
             res.json('auth succsess')
           } else {
@@ -57,7 +133,83 @@ async function loginUser(req, res){
     });
 }
 
+async function updateUser(req, res){
+  const email = req.body.email || 'NULL';
+  const phone = req.body.phone || 'NULL';
+  const link = req.body.link || 'NULL';
+  const gender = req.body.link || 'NULL';
+  const userInfo = req.userInfo;
+
+  connection.query(`UPDATE user SET email = '${email}', phone = '${phone}', link = '${link}', gender='${gender}' 
+  where id = '${userInfo.id}'`, function (error, results, fields) {
+    if (error) {
+      res.status(400)
+      res.json({ error: error })
+    } else {
+      res.status(200)
+      res.json({message:"update success"})
+    }
+  });  
+}
+
+async function changePassword(req, res){
+  const userInfo = req.userInfo;
+  const newPassword = req.body.newPassword;
+
+  const passwordHash = sha256(newPassword);
+
+  connection.query(`UPDATE user SET password = '${passwordHash}' 
+  where id = '${userInfo.id}'`, function (error, results, fields) {
+    if (error) {
+      res.status(400)
+      res.json({ error: error })
+    } else {
+      res.status(200)
+      const cipher = crypto.createCipheriv(algorithm, secretKey, iv);
+      const tokenJson = {
+        'username': userInfo.username,
+        'password': newPassword
+      };
+      const tokenString = JSON.stringify(tokenJson);
+      const encrypted = Buffer.concat([cipher.update(tokenString), cipher.final()]).toString('hex');
+      res.cookie(`token`,encrypted,{
+        maxAge: 24 * 60 * 60 * 1000,
+        secure: false,
+        httpOnly: true,
+        sameSite: 'lax'
+      });
+      res.json({message:"password change success"})
+    }
+  });
+}
+
+async function deleteUser(req, res){
+  const userInfo = req.userInfo;
+  connection.query(`DELETE FROM user where id = '${userInfo.id}'`, 
+  function (error, results, fields) {
+    if (error) {
+      res.status(400)
+      res.json({ error: error })
+    } else {
+      res.status(200)
+      res.clearCookie('token')
+      res.json({message:"account deleted"})
+    }
+  });
+}
+
+async function userInfo(req, res){
+  const userInfo = req.userInfo;
+  res.status(200)
+  res.json(userInfo)
+}
+
 module.exports = {
-    postUser,
-    loginUser
+    createUser,
+    loginUser,
+    checkCookie,
+    userInfo,
+    changePassword,
+    deleteUser,
+    updateUser
   };
--- server/server.js
+++ server/server.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const app = express()
 const routes = require('./routes')
+const cookieParser = require('cookie-parser')
+const helmet = require("helmet");
 
 const cors = require('cors');
 
 app.use(express.json());
+app.use(helmet());
+app.use(cookieParser());
 app.use(
   express.urlencoded({
     extended: true,
@@ -15,9 +19,12 @@ app.use(cors({
   origin: '*'
 }));
 
-app.post('/users', routes.postUser)
-
+app.post('/users', routes.createUser)
 app.post('/login', routes.loginUser)
+app.put('/users', routes.checkCookie, routes.updateUser)
+app.put('/users/password', routes.checkCookie, routes.changePassword)
+app.delete('/users', routes.checkCookie, routes.deleteUser)
+app.get('/userInfo', routes.checkCookie, routes.userInfo)
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
