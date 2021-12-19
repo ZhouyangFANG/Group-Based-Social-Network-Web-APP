adc7c700753901490ea5b8a5e4fcaae20337748a
aaawangyh <wangyh1@shanghaitech.edu.cn>
2021-12-19 17:47:28 -0500

    unittest

--- src/tests/MainView.test.js
+++ src/tests/MainView.test.js
@@ -6,6 +6,15 @@ test('renders learn react link', () => {
   render(<BrowserRouter>
     <MainView />
   </BrowserRouter>);
-  const element = screen.getByText(/Public Groups/i);
+  const element = screen.getByText(/Public Group/i);
   expect(element).toBeInTheDocument();
+  const btn = document.getElementById('groupList');
+  btn.click();
+  const btn1 = document.getElementById('btn1');
+  btn1.click();
+  const btn2 = document.getElementById('btn2');
+  btn2.click();
+  const btn3 = document.getElementById('btn3');
+  btn3.click();
+
 });
