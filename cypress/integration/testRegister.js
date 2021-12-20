9417690743b4c31c0783f4cb7987c1cc2d1dc135
YuchenWang99 <yuchen.wang1220@gmail.com>
2021-12-20 14:52:17 -0500

    update tests

--- cypress/integration/testRegister.js
+++ cypress/integration/testRegister.js
@@ -4,7 +4,7 @@ describe('Test resgiter', () => {
         // const stub = cy.stub()
         // cy.on('window:alert', stub)
 
-        cy.visit('http://localhost:3000/registration');
+        cy.visit('http://localhost:8080/registration');
         cy.get('#userName').type('testCypress').should('have.value', 'testCypress');
         cy.get('#password').type('password').should('have.value', 'password');
         cy.get('#registerBtn').click().then(()=>{
@@ -18,7 +18,7 @@ describe('Test resgiter', () => {
     it('Test registration dup', () => {
         const stub = cy.stub()
         cy.on('window:alert', stub)
-        cy.visit('http://localhost:3000/registration');
+        cy.visit('http://localhost:8080/registration');
         cy.get('#userName').type('testCypress').should('have.value', 'testCypress');
         cy.get('#password').type('password').should('have.value', 'password');
         cy.get('#registerBtn').click().then(()=>{
